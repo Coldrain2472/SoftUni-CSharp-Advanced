5eebd0e0d86c165c63ed671ed7e5bf44b3d30ad7
Coldrain2472 <anelia.peicheva@gmail.com>
2024-01-29 11:19:30 +0200

    My homework on topic "Defining Classes" - Exercise

---
+++ 06. Defining Classes - Exercise/04. Opinion Poll/Family.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> familyMembers = new List<Person>();
+        public Family()
+        {
+            this.familyMembers = new();
+        }
+
+        public List<Person> FamilyMembers
+        {
+            get { return familyMembers; }
+            set { familyMembers = value; }
+        }
+
+
+        //public void AddMember(Person person)
+        //{
+        //    familyMembers.Add(person);
+        //}
+
+        //public Person GetOldestMember()
+        //{
+           
+            
+        //}
+    }
+}
---
+++ 06. Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using DefiningClasses;
+using System.Collections.Generic;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<Person> peopleList = new List<Person>();
+        for (int i = 0; i < n; i++)
+        {
+            string[] currentPerson = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string currentName = currentPerson[0];
+            int currentAge = int.Parse(currentPerson[1]);
+
+            peopleList.Add(new Person(currentName, currentAge));
+        }
+
+        foreach (var person in peopleList.OrderBy(p => p.Name).Where(p => p.Age > 30))
+        {
+            Console.WriteLine($"{person.Name} - {person.Age}");
+        }
+       
+    }
+}
+
