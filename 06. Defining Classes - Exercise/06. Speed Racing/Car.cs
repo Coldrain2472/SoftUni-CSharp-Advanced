92e1e9f3ac5b17e1f8ba96d4e524e6fb99101a23
Coldrain2472 <anelia.peicheva@gmail.com>
2024-01-29 12:23:02 +0200

    My homework on topic "Defining Classes" - Exercise

---
+++ 06. Defining Classes - Exercise/06. Speed Racing/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer, double travelledDistance)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            TravelledDistance = travelledDistance;
+        }
+
+        private string model;
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private double fuelAmount;
+        public double FuelAmount
+        {
+            get { return fuelAmount; }
+            set { fuelAmount = value; }
+        }
+
+        private double fuelConsumptionPerKilometer;
+        public double FuelConsumptionPerKilometer
+        {
+            get { return fuelConsumptionPerKilometer; }
+            set { fuelConsumptionPerKilometer = value; }
+        }
+
+        private double travelledDistance;
+        public double TravelledDistance
+        {
+            get { return travelledDistance; }
+            set { travelledDistance = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            if (distance * FuelConsumptionPerKilometer > fuelAmount)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+            else
+            {
+                FuelAmount -= distance * FuelConsumptionPerKilometer;
+                TravelledDistance += distance;
+            }
+        }
+    }
+}
---
+++ 06. Defining Classes - Exercise/06. Speed Racing/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SpeedRacing
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+            Dictionary<string, Car> cars = new Dictionary<string, Car>();
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                // {model} {fuelAmount} {fuelConsumptionFor1km}
+                string[] carInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = carInfo[0];
+                double fuelAmount = double.Parse(carInfo[1]);
+                double fuelConsumptionFor1km = double.Parse(carInfo[2]);
+                Car car = new Car(model, fuelAmount, fuelConsumptionFor1km, 0);
+                cars.Add(model, car);
+            }
+
+            string commandInfo = string.Empty;
+            while ((commandInfo = Console.ReadLine()) != "End")
+            {
+                // Drive {carModel} {amountOfKm}
+                string[] command = commandInfo.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = command[1];
+                double amountOfKm = double.Parse(command[2]);
+                Car currentCar = cars[model];
+                currentCar.Drive(amountOfKm);
+            }
+            //  {model} {fuelAmount:f2} {distanceTraveled}
+            foreach (var car in cars.Values)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
