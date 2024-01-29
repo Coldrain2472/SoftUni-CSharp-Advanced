96c695b76122f0c8c0c768f112f0ca8b05ab93f3
Coldrain2472 <anelia.peicheva@gmail.com>
2024-01-29 10:30:43 +0200

    My homework on topic "Defining Classes" - Exercise

---
+++ 06. Defining Classes - Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using DefiningClasses;
+using System.Collections.Generic;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        Person firstPerson = new Person();
+        
+        Person secondPerson = new Person();
+        secondPerson.Age = 23;
+        
+        Person thirdPerson = new Person();
+        secondPerson.Name = "Misho";
+        secondPerson.Age = 30;
+
+        Console.WriteLine($"{firstPerson.Name} - {firstPerson.Age}");
+        Console.WriteLine($"{secondPerson.Name} - {secondPerson.Age}");
+        Console.WriteLine($"{thirdPerson.Name} - {thirdPerson.Age}");
+    }
+}
+
