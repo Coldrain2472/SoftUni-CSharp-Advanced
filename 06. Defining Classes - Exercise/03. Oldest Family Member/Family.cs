2bb295caade5c91bceeb5a3ab78038c191a76a86
Coldrain2472 <anelia.peicheva@gmail.com>
2024-01-29 11:02:00 +0200

    My homework on topic "Defining Classes" - Exercise

---
+++ 06. Defining Classes - Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> familyMembers = new List<Person>();
+        public Family()
+        {
+            this.familyMembers = new();
+        }
+
+        public List<Person> FamilyMembers
+        {
+            get { return familyMembers; }
+            set { familyMembers = value; }
+        }
+
+
+        public void AddMember(Person person)
+        {
+            familyMembers.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestMember = familyMembers.MaxBy(x => x.Age);
+            return oldestMember;
+        }
+    }
+}
---
+++ 06. Defining Classes - Exercise/03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using DefiningClasses;
+using System.Collections.Generic;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+       Family familyList = new Family();
+        for (int i = 0; i < n; i++)
+        {
+            string[] currentPerson = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string currentName = currentPerson[0];
+            int currentAge = int.Parse(currentPerson[1]);
+
+            Person person = new Person();
+            person.Name = currentName;
+            person.Age = currentAge;
+            familyList.AddMember(person);
+        }
+       
+        Person oldestPerson = familyList.GetOldestMember();
+        Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+    }
+}
+
