4972e7f86cc7859c691c8d838d986a3aa6b2b4de
Coldrain2472 <anelia.peicheva@gmail.com>
2024-01-29 10:15:23 +0200

    My homework on topic "Defining Classes" -  Exercise

---
+++ 06. Defining Classes - Exercise/01. Define A Class Person/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        public Person()
+        {
+            Name = "Peter";
+            Age = 20;
+        }
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+}
---
+++ 06. Defining Classes - Exercise/01. Define A Class Person/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using DefiningClasses;
+using System.Collections.Generic;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        Person peter = new Person();
+        peter.Name = "Peter";
+        peter.Age = 20;
+        Person george = new Person();
+        george.Name = "George";
+        george.Age = 18;
+        Person jose = new Person();
+        jose.Name = "Jose";
+        jose.Age = 43;
+
+        Console.WriteLine($"{peter.Name} is {peter.Age} years old");
+        Console.WriteLine($"{george.Name} is {george.Age} years old");
+        Console.WriteLine($"{jose.Name} is {jose.Age} years old");
+    }
+}
+
