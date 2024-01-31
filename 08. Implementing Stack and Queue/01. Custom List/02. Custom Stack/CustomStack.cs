7bef1c355d624e6b9c9a11c6d14ceee66e95ad41
Coldrain2472 <anelia.peicheva@gmail.com>
2024-01-31 10:49:37 +0200

    Custom Stack

---
+++ 08. Implementing Stack and Queue/01. Custom List/02. Custom Stack/CustomStack.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class CustomStack
+    {
+
+        public int[] Items { get; set; }
+        public int Count { get; private set; }
+
+        private const int Capacity = 4;
+
+        public CustomStack()
+        {
+            Items = new int[Capacity];
+        }
+
+        public void Push(int value)
+        {
+            if (Items.Length == Count)
+            {
+                Resize();
+            }
+
+            Items[Count] = value;
+            Count++;
+        }
+
+        public int Pop()
+        {
+            if (Items.Length == 0)
+            {
+                throw new InvalidOperationException("CustomStack is empty!");
+            }
+            else if (Count <= Items.Length / 4)
+            {
+                Shrink();
+            }
+
+            int removedElement = Items[Count - 1];
+            Items[Count - 1] = default;
+            Count--;
+            return removedElement;
+        }
+
+        public int Peek()
+        {
+            if (Items.Length == 0)
+            {
+                throw new InvalidOperationException("CustomStack is empty!");
+            }
+
+            return Items[Count - 1];
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            if (Items.Length == 0)
+            {
+                throw new InvalidOperationException("CustomStack is empty!");
+            }
+
+            for (int i = 0; i < Count; i++)
+            {
+                action(Items[i]);
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < Count - 1; i++)
+            {
+                Console.Write(Items[i] + ", ");
+            }
+            Console.WriteLine(Items[Count - 1]);
+        }
+
+        private void Resize()
+        {
+            int[] copy = new int[Items.Length * 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = Items[i];
+            }
+
+            Items = copy;
+        }
+
+        private void Shrink()
+        {
+            int[] copy = new int[Items.Length / 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = Items[i];
+            }
+
+            Items = copy;
+        }
+    }
+}
---
+++ 08. Implementing Stack and Queue/01. Custom List/02. Custom Stack/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            CustomStack stack = new CustomStack();
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] commandInfo = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string commandType = commandInfo[0];
+
+                if (commandType == "Push")
+                {
+                    int value = int.Parse(commandInfo[1]);
+                    stack.Push(value);
+
+                    Console.WriteLine($"Successfully pushed value {value} to the stack!");
+                }
+                else if (commandType == "Pop")
+                {
+                    int value = stack.Pop();
+                    Console.WriteLine($"Successfully popped value {value} from the stack!");
+                }
+                else if (commandType == "Peek")
+                {
+                    int value = stack.Peek();
+                    Console.WriteLine($"Last value is: {value}.");
+                }
+                else if (commandType == "ForEach")
+                {
+                    stack.ForEach(item => Console.Write(item * 2 + " "));
+                    Console.WriteLine();
+                    continue;
+                }
+
+                stack.Print();
+            }
+        }
+    }
+}
