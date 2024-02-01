eeab16dd98baa8422b7a1eb402494035ae8df2f8
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-01 12:15:09 +0200

    My homework on topic "Generics" - Lab

---
+++ 09. Generics - Lab/03. Generic Scale/EqualityScale.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericScale
+{
+    public class EqualityScale<T> where T : IComparable<T>
+    {
+        private T Left;
+        private T Right;
+
+        public EqualityScale(T left, T right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        public bool AreEqual()
+        {
+            return Left.Equals(Right);
+        }
+    }
+}
+
---
+++ 09. Generics - Lab/03. Generic Scale/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            EqualityScale<int> numbers = new EqualityScale<int>(number1, number2);
+            if (numbers.AreEqual())
+            {
+                Console.WriteLine("Numbers are equal");
+            }
+            else
+            {
+                Console.WriteLine("Numbers are not equal");
+            }
+
+            string word1 = Console.ReadLine();
+            string word2 = Console.ReadLine();
+            EqualityScale<string> words = new EqualityScale<string>(word1, word2);
+            if (words.AreEqual())
+            {
+                Console.WriteLine("Words are equal");
+            }
+            else
+            {
+                Console.WriteLine("Words are not equal");
+            }
+        }
+    }
+}
