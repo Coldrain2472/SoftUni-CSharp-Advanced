418ca9692cb4e72e0c57c31df62d9312340069e2
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-01 11:53:26 +0200

    My homework on topic "Generics" - Lab

---
+++ 09. Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> list;
+        
+        public Box() 
+        {
+        list = new List<T>();
+        }
+        
+        public void Add(T input)
+        {
+            list.Insert(0, input);
+        }
+
+        public T Remove()
+        {
+            T elementToRemove = list[0];
+            list.RemoveAt(0);
+            return elementToRemove;
+        }
+
+        public int Count { get { return list.Count; } }
+    }
+}
