3a0b2a4cd1128797856f6a58265cbf58adcb0b61
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-02 12:55:04 +0200

    03. Mail Client

---
+++ Exams Archive/Regular Exam - 21 October 2023/03. Mail Client/Mail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MailClient
+{
+    public class Mail
+    {
+        //given a class Mail with the following properties:
+        //Sender – string
+        //Receiver - string
+        //Body - string
+        //s constructor should receive sender, receiver, and body.
+        //Override the ToString() method in the following format:
+        //"From: {sender} / To: {receiver}
+        //Message: {body}"
+
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Body { get; set; }
+        
+        public Mail(string sender, string receiver, string body)
+        {
+            Sender = sender;
+            Receiver = receiver;
+            Body = body;
+        }
+
+        public override string ToString()
+        {
+            return $"From: {Sender} / To: {Receiver}{Environment.NewLine}Message: {Body}";
+        }
+    }
+}
---
+++ Exams Archive/Regular Exam - 21 October 2023/03. Mail Client/MailBox.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+namespace MailClient
+{
+    public class MailBox
+    {
+        //The class constructor should receive capacity and initialize the Inbox and Archive with new instances of the collections.
+        public MailBox(int capacity)
+        {
+            Capacity = capacity;
+            Inbox = new List<Mail>();
+            Archive = new List<Mail>();
+        }
+        //Capacity - int
+        //Inbox – List<Mail>
+        //Archive – List<Mail>
+        public int Capacity { get; set; }
+        public List<Mail> Inbox { get; set; }
+        public List<Mail> Archive { get; set; }
+
+        public void IncomingMail(Mail mail)
+        {
+            if (Inbox.Count < Capacity)
+            {
+                Inbox.Add(mail);
+            }
+        }
+        public bool DeleteMail(string sender) => Inbox.Remove(Inbox.FirstOrDefault(m => m.Sender == sender));
+        public int ArchiveInboxMessages()
+        {
+            int countMails = Inbox.Count;
+            Archive.AddRange(Inbox);
+            Inbox = new List<Mail>();
+            return countMails;
+        }
+        public string GetLongestMessage() => Inbox.OrderByDescending(m => m.Body.Length).FirstOrDefault().ToString();
+        public string InboxView()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Inbox:");
+            foreach (Mail mail in Inbox)
+            {
+                stringBuilder.AppendLine(mail.ToString().TrimEnd());
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
