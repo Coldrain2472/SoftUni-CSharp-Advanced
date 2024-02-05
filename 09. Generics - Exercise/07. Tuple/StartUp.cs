71f9b205d8f8e2a29e81e1e84521b272100387c2
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-05 13:35:43 +0200

    My homework on topic "Generics" - Exercise

---
+++ 09. Generics - Exercise/07. Tuple/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            // {first name} {last name} {address}
+            string[] nameAndAddress = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            // {name} {liters of beer}
+            string[] nameAndBeerAmount = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            // {integer} {double}
+            string[] intAndDouble = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            CustomTuple<string, string> nameAddress = new($"{nameAndAddress[0]} {nameAndAddress[1]}", nameAndAddress[2]);
+            CustomTuple<string, int> nameBeer = new(nameAndBeerAmount[0], int.Parse(nameAndBeerAmount[1]));
+            CustomTuple<int, double> intDouble = new(int.Parse(intAndDouble[0]), double.Parse(intAndDouble[1]));
+
+            Console.WriteLine(nameAddress);
+            Console.WriteLine(nameBeer);
+            Console.WriteLine(intDouble);
+        }
+    }
+}
+
+
---
+++ 09. Generics - Exercise/08. Threeuple/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Threeuple
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] nameAndAddress = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string[] nameBeerDrunkOrNot = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string[] nameBalanceName = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            CustomThreeuple<string, string, string> person = new(" ", " ", " ");
+
+            if (nameAndAddress.Length > 4)
+            {
+                person =
+                new($"{nameAndAddress[0]} {nameAndAddress[1]}", nameAndAddress[2], $"{nameAndAddress[3]} {nameAndAddress[4]}");
+            }
+            else
+            {
+                person =
+                new($"{nameAndAddress[0]} {nameAndAddress[1]}", nameAndAddress[2], nameAndAddress[3]);
+            }
+
+            CustomThreeuple<string, int, bool> personIsDrunk =
+                new(nameBeerDrunkOrNot[0], int.Parse(nameBeerDrunkOrNot[1]), nameBeerDrunkOrNot[2] == "drunk");
+
+            CustomThreeuple<string, double, string> nameBankBalance =
+                new(nameBalanceName[0], double.Parse(nameBalanceName[1]), nameBalanceName[2]);
+
+            Console.WriteLine(person);
+            Console.WriteLine(personIsDrunk);
+            Console.WriteLine(nameBankBalance);
+        }
+    }
+}
