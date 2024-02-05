da68c08409fa3219bbea5a50da734e8987433a0e
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-05 11:14:20 +0200

    My homework on topic "Generics" - Exercise

---
+++ 09. Generics - Exercise/04. Generic Swap Method Integer/StartUp.cs
@@ -0,0 +1,26 @@
+﻿List<int> items = new List<int>();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    items.Add(int.Parse(Console.ReadLine()));
+}
+int[] indices = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Swap(indices[0], indices[1], items);
+
+foreach (var item in items)
+{
+    Console.WriteLine($"{item.GetType()}: {item}");
+}
+
+static void Swap<T> (int firstIndex, int secondIndex, List<T> items)
+{
+    T word = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = word;
+}
