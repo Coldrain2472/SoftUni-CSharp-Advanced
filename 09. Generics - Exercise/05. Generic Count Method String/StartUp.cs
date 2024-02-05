de565daa62678a7e3a65a6ad165ff289811b2fcb
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-05 11:30:58 +0200

    My homework on topic "Generics" - Exercise

---
+++ 09. Generics - Exercise/05. Generic Count Method String/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericCountMethodStrings
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Box<string> items = new Box<string>();
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                string item = Console.ReadLine();
+                items.Add(item);
+            }
+
+            string itemToCompare = Console.ReadLine();
+
+            Console.WriteLine(items.Compare(itemToCompare));
+        }
+    }
+}
