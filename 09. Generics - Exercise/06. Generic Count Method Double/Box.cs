a379a27dd02374e74df0649571b7d53c8f5f793e
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-05 11:34:17 +0200

    My homework on topic "Generics" - Exercise

---
+++ 09. Generics - Exercise/06. Generic Count Method Double/Box.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCountMethodDouble
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        private List<T> Items { get; set; }
+
+        public Box()
+        {
+            Items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            Items.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+
+            foreach (var item in Items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            T newItem = Items[firstIndex];
+
+            Items[firstIndex] = Items[secondIndex];
+            Items[secondIndex] = newItem;
+        }
+
+        public int Compare(T itemToCompare)
+        {
+            int count = 0;
+
+            foreach (var item in Items)
+            {
+                if (item.CompareTo(itemToCompare) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ 09. Generics - Exercise/06. Generic Count Method Double/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericCountMethodDouble
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Box<double> items = new Box<double>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                double item = double.Parse(Console.ReadLine());
+                items.Add(item);
+            }
+
+            double itemToCompare = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(items.Compare(itemToCompare));
+        }
+    }
+}
