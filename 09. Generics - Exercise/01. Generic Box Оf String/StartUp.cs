dc53e8a084220b662e0416cff855d39bedc0a6a4
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-05 10:43:52 +0200

    My homework on topic "Generics" - Exercise

---
+++ 09. Generics - Exercise/01. Generic Box Оf String/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace BoxOfString
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Box<string> box = new Box<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                box.Add(Console.ReadLine());
+            }
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
