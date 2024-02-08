14f25be7f8f2370a20876e1c6a4ef64ce27d25b6
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-08 13:33:50 +0200

    My homework on topic "Iterators and Comparators" - Exercise

---
+++ 10. Iterators and Comparators - Exercise/07. Custom Comparator/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace MyCustomComparator
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+            Array.Sort(numbers, new CustomComparator());
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ 10. Iterators and Comparators - Exercise/08. Custom Doubly Linked List/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDoublyLinkedList
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+
+            list.AddFirst(3);
+            list.AddFirst(2);
+            list.AddFirst(1);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            DoublyLinkedList<string> listString = new DoublyLinkedList<string>();
+
+            listString.AddFirst("some");
+            listString.AddFirst("random");
+            listString.AddFirst("string");
+
+            foreach (var item in listString)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
