a154cfe448d1e8910d0babb81f851381013e5cb1
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-08 12:42:50 +0200

    My homework on topic "Iterators and Comparators" - Exercise

---
+++ 10. Iterators and Comparators - Exercise/01. ListyIterator/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace LisyIteratorType
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<string> list = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToList();
+            ListyIterator<string> listyIterator = new(list);
+
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command == "Move")
+                {
+                    Console.WriteLine(listyIterator.Move());
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(listyIterator.HasNext());
+                }
+                else if (command == "Print")
+                {
+                    try
+                    {
+                        listyIterator.Print();
+                    }
+                    catch (Exception exception)
+                    {
+
+                        Console.WriteLine(exception.Message);
+                    }
+                    
+                }
+            }
+        }
+    }
+}
+
