72bff6f99257af70482412f0c48154d323df77fe
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-08 13:00:20 +0200

    My homework on topic "Iterators and Comparators" - Exercise

---
+++ 10. Iterators and Comparators - Exercise/02. Collection/StartUp.cs
@@ -0,0 +1,48 @@
+﻿namespace Collection
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<string> list = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Skip(1)
+                 .ToList();
+            ListyIterator<string> listyIterator = new(list);
+
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command == "Move")
+                {
+                    Console.WriteLine(listyIterator.Move());
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(listyIterator.HasNext());
+                }
+                else if (command == "Print")
+                {
+                    try
+                    {
+                        listyIterator.Print();
+                    }
+                    catch (Exception exception)
+                    {
+
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+                else if (command == "PrintAll")
+                {
+                    foreach (var item in listyIterator)
+                    {
+                        Console.Write($"{item} ");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
