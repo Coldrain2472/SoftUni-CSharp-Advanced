194a5b85ae2823c5581ceb56270cb808a0026a0f
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-08 13:08:06 +0200

    My homework on topic "Iterators and Comparators" - Exercise

---
+++ 10. Iterators and Comparators - Exercise/03. Stack/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace Stack
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] commandInfo = command
+                    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string action = commandInfo[0];
+
+                if (action == "Push")
+                {
+                    int[] itemsToPush = commandInfo
+                        .Skip(1)
+                        .Select(int.Parse)
+                        .ToArray();
+
+                    foreach (var item in itemsToPush)
+                    {
+                        stack.Push(item);
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
