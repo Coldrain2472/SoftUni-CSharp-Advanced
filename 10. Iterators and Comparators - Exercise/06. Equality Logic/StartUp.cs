cdfbe0666826b78f41a7bf9e9812f962cb296c4d
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-08 13:24:01 +0200

    My homework on topic "Iterators and Comparators" - Exercise

---
+++ 10. Iterators and Comparators - Exercise/06. Equality Logic/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace EqualityLogic
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            HashSet<Person> hashSet = new HashSet<Person>();
+            SortedSet<Person> sortedSet = new SortedSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person()
+                {
+                    Name = personInfo[0],
+                    Age = int.Parse(personInfo[1])
+                };
+
+                hashSet.Add(person);
+                sortedSet.Add(person);
+            }
+
+            Console.WriteLine(hashSet.Count);
+            Console.WriteLine(sortedSet.Count);
+        }
+    }
+}
