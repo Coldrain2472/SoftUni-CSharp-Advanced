8f1f8daa4b5432a03dd02146503c9fb39074ff91
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-15 14:33:40 +0200

    Update 02.BlindMansBuff.cs

--- Exams Archive/Regular Exam - 18 February 2023/02.BlindMansBuff.cs
+++ Exams Archive/Regular Exam - 18 February 2023/02.BlindMansBuff.cs
@@ -1,77 +1,104 @@
-int[] dimensions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int[] dimensions = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
 
 int rows = dimensions[0];
 int cols = dimensions[1];
 
-string[,] playground = new string[rows, cols];
+string[,] matrix = new string[rows, cols];
 int playerRow = 0;
 int playerCol = 0;
+int playersCount = 0;
 
 for (int row = 0; row < rows; row++)
 {
-    string[] playgroundInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
-
+    string[] matrixInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
     for (int col = 0; col < cols; col++)
     {
-        playground[row, col] = playgroundInfo[col];
-        if (playgroundInfo[col] == "B")
+        matrix[row, col] = matrixInfo[col];
+        if (matrix[row, col] == "B")
         {
             playerRow = row;
             playerCol = col;
         }
+        else if (matrix[row, col] == "P")
+        {
+            playersCount++;
+        }
     }
 }
 
-int touchedOpponents = 0;
-int movesMade = 0;
 string command = string.Empty;
+int touchedPlayers = 0;
+int movesMade = 0;
 
 while ((command = Console.ReadLine()) != "Finish")
 {
-    if (command == "up")
+    bool moveIsMade = false;
+    if (command == "up" && IsPositionValid(playerRow - 1, playerCol, rows, cols))
     {
-        if (playerRow - 1 >= 0 && playground[playerRow - 1, playerCol] != "O")
+        if (matrix[playerRow - 1, playerCol] != "O")
         {
             playerRow--;
             movesMade++;
+            moveIsMade = true;
         }
     }
-    else if (command == "down")
+    else if (command == "down" && IsPositionValid(playerRow + 1, playerCol, rows, cols))
     {
-        if (playerRow + 1 < rows && playground[playerRow + 1, playerCol] != "O")
+        if (matrix[playerRow + 1, playerCol] != "O")
         {
             playerRow++;
             movesMade++;
+            moveIsMade = true;
         }
     }
-    else if (command == "left")
+    else if (command == "right" && IsPositionValid(playerRow, playerCol + 1, rows, cols))
     {
-        if (playerCol - 1 >= 0 && playground[playerRow, playerCol - 1] != "O")
-        {
-            playerCol--;
-            movesMade++;
-        }
-    }
-    else if (command == "right")
-    {
-        if (playerCol + 1 < cols && playground[playerRow, playerCol + 1] != "O")
+        if (matrix[playerRow, playerCol + 1] != "O")
         {
             playerCol++;
             movesMade++;
+            moveIsMade = true;
         }
     }
-
-    if (playground[playerRow, playerCol] == "P")
+    else if (command == "left" && IsPositionValid(playerRow, playerCol - 1, rows, cols))
     {
-        touchedOpponents++;
-        playground[playerRow, playerCol] = "-";
+        if (matrix[playerRow, playerCol - 1] != "O")
+        {
+            playerCol--;
+            movesMade++;
+            moveIsMade = true;
+        }
+    }
+    else
+    {
+        continue;
     }
 
-    if (touchedOpponents == 3)
+    if (moveIsMade)
     {
-        break;
+        if (matrix[playerRow, playerCol] == "P")
+        {
+            touchedPlayers++;
+            if (playersCount == touchedPlayers)
+            {
+                break;
+            }
+        }
     }
 }
 
 Console.WriteLine("Game over!");
-Console.WriteLine($"Touched opponents: {touchedOpponents} Moves made: {movesMade}");
+Console.WriteLine($"Touched opponents: {touchedPlayers} Moves made: {movesMade}");
+
+
+static bool IsPositionValid(int row, int col, int rows, int cols)
+{
+    if (row >= 0 && row < rows && col >= 0 && col < cols)
+    {
+        return true;
+    }
+    return false;
+}
